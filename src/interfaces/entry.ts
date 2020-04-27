8cc23e392159a10020b0329cc3f2b16725095572
brettm42 <brettm42@gmail.com>
2020-04-27 11:16:51 -0700

    add debug validation property to entry model

--- src/interfaces/entry.ts
+++ src/interfaces/entry.ts
@@ -1,6 +1,9 @@
+import { ValidationFlag } from '../infrastructure/shared';
+
 export interface Entry {
   readonly id: number;
   readonly name: string;
   readonly description?: string;
   readonly archive?: boolean;
+  validation?: ReadonlyArray<Map<string, ValidationFlag>>;
 }
